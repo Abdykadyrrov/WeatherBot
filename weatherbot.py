142b702c73673c48ef77db11634eb1efa7145ba1
Abdykadyrrov <103096307+Abdykadyrrov@users.noreply.github.com>
2022-05-21 19:23:17 +0600

    Add files via upload

---
+++ weatherbot.py
@@ -0,0 +1,74 @@
+import requests
+import datetime
+from config import tg_bot_token, open_weather_token
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+
+bot = Bot(token=tg_bot_token)
+dp = Dispatcher(bot)
+
+
+
+@dp.message_handler(commands=["start"])
+async def start_command(message: types.Message):
+    await message.reply("Привет! Напиши мне название города и я пришлю сводку погоды!")
+
+
+
+@dp.message_handler()
+async def get_weather(message: types.Message):
+    code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+    }
+
+    try:
+        r = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
+        )
+        data = r.json()
+
+        city = data["name"]
+
+        cur_weather = data["main"]["temp"]
+        cur_weather1 = int(cur_weather)
+        if cur_weather1 > 30:
+            feedback = "На улице жарищее! Далой одежду!!"
+        elif cur_weather1 < 30 and cur_weather1 > 15:
+            feedback = "На улице тепло! Нурислам советует одеться более свободно."
+        elif cur_weather1 < 15 and cur_weather1 > 0:
+            feedback = "На улице прохладно! Нурислам советует одеться потеплее!!"
+        elif cur_weather1 < 0 and cur_weather1 > -15:
+            feedback = "На улице холодно! Нурислам напоминает надеть кологтки!!"
+        elif cur_weather1 < -15 and cur_weather1 > -30:
+            feedback = "На улице холодрыга! Нурислам совету одеться максимально тепло, будто едешь на Северный полюс!!"
+
+
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = "Посмотри в окно, не пойму что там за погода!"
+
+
+
+        await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+              f"Погода в городе: {city}\nТемпература: {cur_weather1}°C {wd}\n Совет: {feedback}\n"
+
+              f"Хорошего дня!"
+              )
+
+
+    except:
+        await message.reply("\U00002620 Проверьте название города \U00002620")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
